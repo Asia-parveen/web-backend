69bbbc04376a50acdc80336ce7a57ebd45dfecbf
Asia-parveen <beautyhealthytips177@gmail.com>
2025-04-21 22:49:29 +0500

    added

--- app.mjs
+++ app.mjs
@@ -4,13 +4,26 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectToDb from "./db/db.js";
 import authRoutes from "./routes/authRoutes.js";
-// const port = 3000
+ const port = 5000
 dotenv.config();
 
 const app = express();
 connectToDb();
 
-app.use(cors());
+// app.use(cors());
+app.use(
+	cors({
+		origin: ['http://localhost:5174',
+			 'http://localhost:5173',
+             'https://web-frontend-rho-flax.vercel.app/',
+			
+			
+			],
+		methods: ['GET', 'PUT', 'POST', 'DELETE'],
+		credentials: true,
+		allowedHeaders: ['Content-Type', 'Authorization'],
+	}),
+);
 app.use(express.json());
 
 // app.get('/', (req, res) => {
@@ -19,9 +32,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
